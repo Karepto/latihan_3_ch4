9ad9913b01c5d522c7e33ec160f4e44db28ca1df
Karepto <renggadwipribadi@gmail.com>
2024-03-25 22:00:55 +0700

    Initial commit

---
+++ sqlbuat.sql
@@ -0,0 +1,10 @@
+-- Active: 1710511749892@@127.0.0.1@5432@movies
+CREATE DATABASE movies;
+
+CREATE Table movies (
+    id BIGINT PRIMARY KEY,
+    title VARCHAR(255),
+    director VARCHAR(255),
+    release_year INT,
+    is_available BOOLEAN
+)
